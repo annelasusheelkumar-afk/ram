6abccdb06e38dd55cd083188b3f21bb4c3470963
SusheelKumar Annela <annelasusheelkumar@gmail.com>
2025-10-18 07:33:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2301500788-e948e",
+  "appId": "1:253318450593:web:e41acc1af8d80aacf50fe0",
+  "apiKey": "AIzaSyA18pgO65X8C1C1PgpJQl88BCJZ9uU9XMA",
+  "authDomain": "studio-2301500788-e948e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "253318450593"
+};
